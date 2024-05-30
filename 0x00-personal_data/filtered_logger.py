ca3e3f2ee1a0d563841185380837b57f64132905
Pronothurah <onsongojosphat28@gmail.com>
2024-05-30 13:54:38 +0300

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""filter_datum module"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated:"""
+    pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
+    return re.sub(pattern, lambda m:
+                  f"{m.group(0).split('=')[0]}={redaction}", message)
