0d9561993fffcaa4f0115eb4e0c9ac6edff0e91e
Pronothurah <onsongojosphat28@gmail.com>
2024-05-30 15:52:24 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,6 +23,7 @@ def filter_datum(
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
 
+
 def get_logger() -> logging.Logger:
     """Creates a new logger for user data.
     """
@@ -72,7 +73,6 @@ def main():
             args = ("user_data", logging.INFO, None, None, msg, None, None)
             log_record = logging.LogRecord(*args)
             info_logger.handle(log_record)
-
 
 
 class RedactingFormatter(logging.Formatter):
