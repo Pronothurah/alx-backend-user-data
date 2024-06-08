3f7884dbb7e30032a1c8cac512bddbbf7db135ea
Pronothurah <onsongojosphat28@gmail.com>
2024-06-08 23:15:24 +0300

    Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,10 @@
 """Basic authentication module for the API.
 """
 from uuid import uuid4
+from flask import request
 
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -24,3 +26,9 @@ class SessionAuth(Auth):
         """
         if isinstance(session_id, str):
             return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """Retrieves the user associated with the request.
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
