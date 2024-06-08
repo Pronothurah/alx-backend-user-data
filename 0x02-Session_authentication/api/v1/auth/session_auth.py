07991a8f031b312d5fe360e7f3fba36ce563f80e
Pronothurah <onsongojosphat28@gmail.com>
2024-06-08 22:34:06 +0300

    Task 3 complete

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API.
 """
+from uuid import uuid4
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Basic authentication class.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
