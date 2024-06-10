7a0748b7792dbdeec5dba4aafdfdb7b05d2bf50a
Pronothurah <onsongojosphat28@gmail.com>
2024-06-10 13:14:57 +0300

    Task 0 complete User Model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module SQLAlchemy model named User"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Represents a record from the `user` table.
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(256), nullable=False, unique=True)
+    hashed_password = Column(String(256), nullable=False)
+    session_id = Column(String(256), nullable=False)
+    reset_token = Column(String(256), nullable=False)
